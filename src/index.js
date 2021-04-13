e47f904166d5f7d3c2426fa2be18a76938617c34
emmanuel2612 <ojijimanuel@hotmail.com>
2021-04-13 01:07:39 +0100

    Create gh-pages branch with static content

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { DataLayer } from "./DataLayer";
 import reducer, { initialState } from "./reducer";
 
