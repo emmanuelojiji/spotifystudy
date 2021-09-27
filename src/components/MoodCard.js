059abd4847216b667cbe7a7c7152ac1a77931970
emmanuel2612 <ojijimanuel@hotmail.com>
2021-09-27 11:16:46 +0100

    Working sounds

--- src/components/MoodCard.js
+++ src/components/MoodCard.js
@@ -5,29 +5,36 @@ import { useState } from "react";
 export default function MoodCard(props) {
   const [isHover, setIsHover] = useState(false);
 
-
   return (
     <button
       className="mood-card"
       onClick={props.playSound}
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
+      style={{ border: props.showBorder }}
     >
       <figure
         style={{ backgroundImage: `url('${props.backgroundImage}')` }}
       ></figure>
       <h1>{props.name}</h1>
 
-        <div className="play-button-container" style={{opacity: isHover && '1', display: props.hidePlayButton && 'none'}}>
-          <i className="fas fa-play"/>
-        </div>
+      <div
+        className="play-button-container"
+        style={{
+          opacity: isHover && "1",
+          display: props.hidePlayButton && "none",
+        }}
+      >
+        <i className="fas fa-play" />
+      </div>
 
-        <div className="pause-button-container" style={{display: props.showPauseButton && 'flex'}} onClick={props.pauseSound}>
-          <i className="fas fa-pause"/>
-        </div>
-
-
-
+      <div
+        className="pause-button-container"
+        style={{ display: props.showPauseButton && "flex" }}
+        onClick={props.pauseSound}
+      >
+        <i className="fas fa-pause" />
+      </div>
     </button>
   );
 }
--- src/pages/MoodPage.js
+++ src/pages/MoodPage.js
@@ -6,27 +6,37 @@ import { useRef } from "react";
 import { MoodCardInfo } from "./MoodCardInfo";
 
 export default function MoodPage(props) {
-  const audioRef = useRef(null);
+  const RainAudioRef = useRef(null);
+  const OceanAudioRef = useRef(null);
+  const ThunderAudioRef = useRef(null);
+  const WaterfallAudioRef = useRef(null);
+  const NightForestAudioRef = useRef(null);
 
-  const [currentSound, setCurrentSound] = useState(null);
-  const [isSoundPlaying, setIsSoundPlaying] = useState(false);
+  const [isRainSoundPlaying, setIsRainSoundPlaying] = useState(false);
+  const [isOceanSoundPlaying, setIsOceanSoundPlaying] = useState(false);
+  const [isThunderSoundPlaying, setIsThunderSoundPlaying] = useState(false);
+  const [isWaterfallSoundPlaying, setIsWaterfallSoundPlaying] = useState(false);
+  const [isNightForestSoundPlaying, setIsNightForestSoundPlaying] = useState(false);
 
-  useEffect(() => {
-    console.log(currentSound);
-    console.log(isSoundPlaying);
-  });
+  const borderStyle = "solid 1px #666666";
 
   return (
     <>
-      {/*<audio loop="true" className="audio" ref={audioRef}>
-        <source src={currentSound} />
-  </audio>*/}
-
-      {MoodCardInfo.map((audioMap) => (
-        <audio loop="true" className="audio" ref={audioRef}>
-          <source src={audioMap.sound} />
-        </audio>
-      ))}
+      <audio loop="true" className="audio" ref={RainAudioRef}>
+        <source src={MoodCardInfo[0].sound} />
+      </audio>
+      <audio loop="true" className="audio" ref={OceanAudioRef}>
+        <source src={MoodCardInfo[1].sound} />
+      </audio>
+      <audio loop="true" className="audio" ref={ThunderAudioRef}>
+        <source src={MoodCardInfo[2].sound} />
+      </audio>
+      <audio loop="true" className="audio" ref={WaterfallAudioRef}>
+        <source src={MoodCardInfo[3].sound} />
+      </audio>
+      <audio loop="true" className="audio" ref={NightForestAudioRef}>
+        <source src={MoodCardInfo[4].sound} />
+      </audio>
 
       <div className="mood-container">
         <header>
@@ -38,28 +48,102 @@ export default function MoodPage(props) {
         <div className="nature-container">
           <h1 className="row-title">Nature</h1>
           <section className="mood-row">
-            {MoodCardInfo.map((info) => (
-              <>
-                <MoodCard
-                  name={info.name}
-                  backgroundImage={info.image}
-                  playSound={() => {
-                    setCurrentSound(info.sound);
-                    audioRef.current.load();
-                    audioRef.current.play();
-                    audioRef.current.play && setIsSoundPlaying(true);
-                  }}
-                  pauseSound={(event) => {
-                    audioRef.current.pause();
-                    setIsSoundPlaying(false);
-                    console.log("paused");
-                    event.stopPropagation();
-                  }}
-                  hidePlayButton={isSoundPlaying}
-                  showPauseButton={isSoundPlaying}
-                />
-              </>
-            ))}
+            <MoodCard
+              name={MoodCardInfo[0].name}
+              backgroundImage={MoodCardInfo[0].image}
+              playSound={() => {
+                RainAudioRef.current.load();
+                RainAudioRef.current.play();
+                RainAudioRef.current.play && setIsRainSoundPlaying(true);
+              }}
+              pauseSound={(event) => {
+                RainAudioRef.current.pause();
+                setIsRainSoundPlaying(false);
+                console.log("paused");
+                event.stopPropagation();
+              }}
+              hidePlayButton={isRainSoundPlaying}
+              showPauseButton={isRainSoundPlaying}
+              showBorder={isRainSoundPlaying && borderStyle}
+            />
+
+            <MoodCard
+              name={MoodCardInfo[1].name}
+              backgroundImage={MoodCardInfo[1].image}
+              playSound={() => {
+                OceanAudioRef.current.load();
+                OceanAudioRef.current.play();
+                OceanAudioRef.current.play && setIsOceanSoundPlaying(true);
+              }}
+              pauseSound={(event) => {
+                OceanAudioRef.current.pause();
+                setIsOceanSoundPlaying(false);
+                console.log("paused");
+                event.stopPropagation();
+              }}
+              hidePlayButton={isOceanSoundPlaying}
+              showPauseButton={isOceanSoundPlaying}
+              showBorder={isOceanSoundPlaying && borderStyle}
+            />
+
+            <MoodCard
+              name={MoodCardInfo[2].name}
+              backgroundImage={MoodCardInfo[2].image}
+              playSound={() => {
+                ThunderAudioRef.current.load();
+                ThunderAudioRef.current.play();
+                ThunderAudioRef.current.play && setIsThunderSoundPlaying(true);
+              }}
+              pauseSound={(event) => {
+                ThunderAudioRef.current.pause();
+                setIsThunderSoundPlaying(false);
+                console.log("paused");
+                event.stopPropagation();
+              }}
+              hidePlayButton={isThunderSoundPlaying}
+              showPauseButton={isThunderSoundPlaying}
+              showBorder={isThunderSoundPlaying && borderStyle}
+            />
+
+            <MoodCard
+              name={MoodCardInfo[3].name}
+              backgroundImage={MoodCardInfo[3].image}
+              playSound={() => {
+                WaterfallAudioRef.current.load();
+                WaterfallAudioRef.current.play();
+                WaterfallAudioRef.current.play &&
+                  setIsWaterfallSoundPlaying(true);
+              }}
+              pauseSound={(event) => {
+                WaterfallAudioRef.current.pause();
+                setIsWaterfallSoundPlaying(false);
+                console.log("paused");
+                event.stopPropagation();
+              }}
+              hidePlayButton={isWaterfallSoundPlaying}
+              showPauseButton={isWaterfallSoundPlaying}
+              showBorder={isWaterfallSoundPlaying && borderStyle}
+            />
+
+            <MoodCard
+              name={MoodCardInfo[4].name}
+              backgroundImage={MoodCardInfo[4].image}
+              playSound={() => {
+                NightForestAudioRef.current.load();
+                NightForestAudioRef.current.play();
+                NightForestAudioRef.current.play &&
+                  setIsNightForestSoundPlaying(true);
+              }}
+              pauseSound={(event) => {
+                NightForestAudioRef.current.pause();
+                setIsNightForestSoundPlaying(false);
+                console.log("paused");
+                event.stopPropagation();
+              }}
+              hidePlayButton={isNightForestSoundPlaying}
+              showPauseButton={isNightForestSoundPlaying}
+              showBorder={isNightForestSoundPlaying && borderStyle}
+            />
           </section>
         </div>
       </div>
